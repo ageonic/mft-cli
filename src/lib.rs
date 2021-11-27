7b9f786340de6c8660304291ad0772293de7588f
ageonic <angeorge.dev@gmail.com>
2021-11-27 12:25:39 -0500

    Create Tracker struct to make implementation easier

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,65 @@
 #[macro_use]
 extern crate prettytable;
 
+use reqwest::header::USER_AGENT;
 use serde::Deserialize;
+use std::error::Error;
+
+const BASE_URL: &str =
+    "https://www.mattressfirm.com/track/_next/data/1PV9Lq-1gfywGk2GkInta/results.json";
+const USER_AGENT_VALUE: &str =
+    "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0";
+
+pub struct Tracker {
+    order_number: String,
+    form_secondary_detail_name: String,
+    form_secondary_detail_value: String,
+}
+
+impl Tracker {
+    pub fn new(order_number: String, verification_detail: Verification) -> Tracker {
+        let (form_secondary_detail_name, form_secondary_detail_value) = match verification_detail {
+            Verification::LastName(last_name) => (String::from("formLastName"), last_name),
+            Verification::ZipCode(zip) => (String::from("formZipCode"), zip),
+            Verification::Phone(phone) => (String::from("formPhone"), phone),
+        };
+        Tracker {
+            order_number,
+            form_secondary_detail_name,
+            form_secondary_detail_value,
+        }
+    }
+
+    pub fn get_shipment_details(&self) -> Result<Shipment, Box<dyn Error>> {
+        let url = format!(
+            "{}?orderNumber={}&{}={}",
+            BASE_URL,
+            self.order_number,
+            self.form_secondary_detail_name,
+            self.form_secondary_detail_value
+        );
+
+        let client = reqwest::blocking::Client::new();
+        let response = client
+            .get(url)
+            .header(USER_AGENT, USER_AGENT_VALUE)
+            .send()?;
+        let data = response.text()?;
+
+        let props: serde_json::Value = serde_json::from_str(&data).unwrap();
+
+        let shipment: Shipment =
+            serde_json::from_value(props["pageProps"]["data"]["shipments"][0].clone())?;
+
+        Ok(shipment)
+    }
+}
+
+pub enum Verification {
+    LastName(String),
+    ZipCode(String),
+    Phone(String),
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Shipment {
@@ -23,7 +81,7 @@ pub struct Shipment {
 }
 
 #[derive(Debug, Deserialize)]
-struct TrackingInfo {
+pub struct TrackingInfo {
     carrier: Option<String>,
     #[serde(rename = "expectedTime")]
     expected_time: Option<String>,
@@ -54,7 +112,7 @@ impl Shipment {
         table.printstd();
     }
 
-    fn show_verbose(&self) {
+    pub fn show_verbose(&self) {
         let table = table!(
             [
                 "Shipment ID",
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use reqwest::header::USER_AGENT;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://www.mattressfirm.com/track/_next/data/1PV9Lq-1gfywGk2GkInta/results.json?orderNumber=&formZipCode=";
-    let user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0";
-
-    let client = reqwest::blocking::Client::new();
-    let response = client.get(url).header(USER_AGENT, user_agent).send()?;
-    let data = response.text()?;
-
-    let props: serde_json::Value = serde_json::from_str(&data).unwrap();
-
-    let shipment: mft::Shipment =
-        serde_json::from_value(props["pageProps"]["data"]["shipments"][0].clone())?;
-
     Ok(())
 }
